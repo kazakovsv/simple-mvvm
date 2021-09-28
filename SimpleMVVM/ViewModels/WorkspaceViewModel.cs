33cf15b061d0acc7dcfe278a4089d5d7625671d7
Stanislav Kazakov <skazakov.workmail@gmail.com>
2021-09-28 22:27:53 +0300

    Add a workspace view model

---
+++ SimpleMVVM/ViewModels/WorkspaceViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+using SimpleMVVM.Commands;
+
+namespace SimpleMVVM.ViewModels
+{
+    public abstract class WorkspaceViewModel : BaseViewModel
+    {
+        protected WorkspaceViewModel()
+        {
+        }
+
+        private DelegateCommand m_closeCommand;
+
+        public ICommand CloseCommand
+        {
+            get
+            {
+                if (m_closeCommand == null)
+                {
+                    m_closeCommand = new DelegateCommand(parameter => RaiseRequestCloseEvent());
+                }
+
+                return m_closeCommand;
+            }
+        }
+
+        public event EventHandler RequestClose;
+
+        protected void RaiseRequestCloseEvent()
+        {
+            RequestClose?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
