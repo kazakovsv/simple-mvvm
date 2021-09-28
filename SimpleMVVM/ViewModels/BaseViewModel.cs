4d3ae159bfbb95e5ab68857a852aea603a19d770
Stanislav Kazakov <skazakov.workmail@gmail.com>
2021-09-28 22:10:39 +0300

    Create a basic view model

---
+++ SimpleMVVM/ViewModels/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SimpleMVVM.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        protected BaseViewModel()
+        {
+        }
+
+        public virtual string DisplayName { get; protected set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyName([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
