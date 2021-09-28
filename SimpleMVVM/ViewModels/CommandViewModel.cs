d3fa68ecfefcc24569232b922378c666eaa110e4
Stanislav Kazakov <skazakov.workmail@gmail.com>
2021-09-28 22:20:56 +0300

    Add a view model for commands

---
+++ SimpleMVVM/ViewModels/CommandViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SimpleMVVM.ViewModels
+{
+    public class CommandViewModel : BaseViewModel
+    {
+        public CommandViewModel(string displayName, ICommand command)
+        {
+            DisplayName = displayName ?? throw new ArgumentNullException(nameof(displayName));
+            Command = command ?? throw new ArgumentNullException(nameof(command));
+        }
+
+        public ICommand Command { get; private set; }
+    }
+}
