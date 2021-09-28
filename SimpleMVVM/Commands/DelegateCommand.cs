7f046fa9fd49864cce3c59ecd43622b1772512a1
Stanislav Kazakov <skazakov.workmail@gmail.com>
2021-09-28 22:16:35 +0300

    Create an implementation of the ICommand interface

---
+++ SimpleMVVM/Commands/DelegateCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SimpleMVVM.Commands
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action<object> m_execute;
+        private readonly Predicate<object> m_canExecute;
+
+        public DelegateCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            m_execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            m_canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return m_canExecute == null || m_canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            m_execute(parameter);
+        }
+    }
+}
