bd395fe5e86a0ef0d5e028139377449540c65d87
Stanislav Kazakov <skazakov.workmail@gmail.com>
2021-09-29 21:26:05 +0300

    Add a main view and resources for it

---
+++ SimpleMVVM/Views/MainView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace SimpleMVVM.Views
+{
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
