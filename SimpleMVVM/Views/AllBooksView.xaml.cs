f50043a005639f09b81e2c6a70d4563a1fb6296c
Stanislav Kazakov <skazakov.workmail@gmail.com>
2021-09-29 18:19:51 +0300

    Add a view for all books

---
+++ SimpleMVVM/Views/AllBooksView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace SimpleMVVM.Views
+{
+    public partial class AllBooksView : UserControl
+    {
+        public AllBooksView()
+        {
+            InitializeComponent();
+        }
+    }
+}
