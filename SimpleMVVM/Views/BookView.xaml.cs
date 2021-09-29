5a1d5978afe4c1357973f1ec6cb31b4c2fb56ed0
Stanislav Kazakov <skazakov.workmail@gmail.com>
2021-09-29 17:37:26 +0300

    Add a view for each book

---
+++ SimpleMVVM/Views/BookView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace SimpleMVVM.Views
+{
+    public partial class BookView : UserControl
+    {
+        public BookView()
+        {
+            InitializeComponent();
+        }
+    }
+}
